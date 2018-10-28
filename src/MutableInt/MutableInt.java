1c237ee0379dd96d23bc5a4e6c31b0f451b134dd
Sergi Simón <sergisiba@gmail.com>
2018-10-28 19:30:58 +0100

    Projecte inicial fet.  [TODO]: falta revisar els tests i el codi. Falta fer la documentació. Trobat error en els testos

--- src/MutableInt/MutableInt.java
+++ src/MutableInt/MutableInt.java
@@ -3,18 +3,79 @@ package MutableInt;
 public class MutableInt {
     private int i;
 
+    /**
+     * Constructro of the Class
+     *
+     * @param i sets the number stored to it.
+     */
     public MutableInt(int i) {
         this.i = i;
     }
 
+    /**
+     * Getter of the number stored
+     *
+     * @return the number stored
+     */
     public int getI() {
         return i;
     }
 
+    /**
+     * Setter of the number stored. Makes the class mutable
+     * @param i new element stored
+     */
     public void setI(int i) {
         this.i = i;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * <p>
+     * The {@code equals} method implements an equivalence relation
+     * on non-null object references:
+     * <ul>
+     * <li>It is <i>reflexive</i>: for any non-null reference value
+     * {@code x}, {@code x.equals(x)} should return
+     * {@code true}.
+     * <li>It is <i>symmetric</i>: for any non-null reference values
+     * {@code x} and {@code y}, {@code x.equals(y)}
+     * should return {@code true} if and only if
+     * {@code y.equals(x)} returns {@code true}.
+     * <li>It is <i>transitive</i>: for any non-null reference values
+     * {@code x}, {@code y}, and {@code z}, if
+     * {@code x.equals(y)} returns {@code true} and
+     * {@code y.equals(z)} returns {@code true}, then
+     * {@code x.equals(z)} should return {@code true}.
+     * <li>It is <i>consistent</i>: for any non-null reference values
+     * {@code x} and {@code y}, multiple invocations of
+     * {@code x.equals(y)} consistently return {@code true}
+     * or consistently return {@code false}, provided no
+     * information used in {@code equals} comparisons on the
+     * objects is modified.
+     * <li>For any non-null reference value {@code x},
+     * {@code x.equals(null)} should return {@code false}.
+     * </ul>
+     * <p>
+     * The {@code equals} method for class {@code Object} implements
+     * the most discriminating possible equivalence relation on objects;
+     * that is, for any non-null reference values {@code x} and
+     * {@code y}, this method returns {@code true} if and only
+     * if {@code x} and {@code y} refer to the same object
+     * ({@code x == y} has the value {@code true}).
+     * <p>
+     * Note that it is generally necessary to override the {@code hashCode}
+     * method whenever this method is overridden, so as to maintain the
+     * general contract for the {@code hashCode} method, which states
+     * that equal objects must have equal hash codes.
+     *
+     * @param obj the reference to any implementation of Stack<E> where E
+     *            is the type of the elements of the SharedStack passed.
+     * @return {@code true} if this object is the same as the obj
+     *          argument; {@code false} otherwise.
+     * @see #hashCode()
+     * @see java.util.HashMap
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj != null) {
